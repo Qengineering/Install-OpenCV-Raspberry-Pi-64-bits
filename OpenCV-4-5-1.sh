eef79a6be2273f50db8d46dcc84640ed0e4d35a1
Q-engineering <info@qengineering.eu>
2021-06-11 12:06:25 +0200

    Installation script OpenCV 4.5.1

---
+++ OpenCV-4-5-1.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+echo "Installing OpenCV 4.5.1 on your Raspberry Pi 64-bit OS"
+echo "It will take minimal 1.5 hour !"
+cd ~
+# install the dependencies
+sudo apt-get install -y build-essential cmake git unzip pkg-config
+sudo apt-get install -y libjpeg-dev libpng-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libgtk2.0-dev libcanberra-gtk* libgtk-3-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y python3-dev python3-numpy python3-pip
+sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev
+sudo apt-get install -y libv4l-dev v4l-utils
+sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
+sudo apt-get install -y liblapack-dev gfortran libhdf5-dev
+sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
+sudo apt-get install -y protobuf-compiler
+
+# download the latest version
+cd ~ 
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.1.zip 
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.1.zip 
+# unpack
+unzip opencv.zip 
+unzip opencv_contrib.zip 
+# some administration to make live easier later on
+mv opencv-4.5.1 opencv
+mv opencv_contrib-4.5.1 opencv_contrib
+# clean up the zip files
+rm opencv.zip
+rm opencv_contrib.zip
+
+# set install dir
+cd ~/opencv
+mkdir build
+cd build
+
+# run cmake
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+-D ENABLE_NEON=ON \
+-D WITH_OPENMP=ON \
+-D WITH_OPENCL=OFF \
+-D BUILD_TIFF=ON \
+-D WITH_FFMPEG=ON \
+-D WITH_TBB=ON \
+-D BUILD_TBB=ON \
+-D BUILD_TESTS=OFF \
+-D WITH_EIGEN=OFF \
+-D WITH_GSTREAMER=OFF \
+-D WITH_V4L=ON \
+-D WITH_LIBV4L=ON \
+-D WITH_VTK=OFF \
+-D WITH_QT=OFF \
+-D OPENCV_ENABLE_NONFREE=ON \
+-D INSTALL_C_EXAMPLES=OFF \
+-D INSTALL_PYTHON_EXAMPLES=OFF \
+-D BUILD_NEW_PYTHON_SUPPORT=ON \
+-D BUILD_opencv_python3=TRUE \
+-D OPENCV_GENERATE_PKGCONFIG=ON \
+-D BUILD_EXAMPLES=OFF ..
+
+# run make
+make -j4
+sudo make install
+sudo ldconfig
+
+# cleaning (frees 300 MB)
+make clean
+sudo apt-get update
