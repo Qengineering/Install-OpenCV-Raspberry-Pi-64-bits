7476681fd90dd3f2282e68a77d0d5ab0341b0d93
Q-engineering <info@qengineering.eu>
2021-12-01 15:28:01 +0100

    GStreamer update

--- OpenCV-4-5-4.sh
+++ OpenCV-4-5-4.sh
@@ -8,6 +8,8 @@ sudo apt-get install -y build-essential cmake git unzip pkg-config
 sudo apt-get install -y libjpeg-dev libtiff-dev libpng-dev
 sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y libgtk2.0-dev libcanberra-gtk* libgtk-3-dev
+sudo apt-get install -y libgstreamer1.0-dev gstreamer1.0-gtk3
+sudo apt-get install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-gl
 sudo apt-get install -y libxvidcore-dev libx264-dev
 sudo apt-get install -y python3-dev python3-numpy python3-pip
 sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev
