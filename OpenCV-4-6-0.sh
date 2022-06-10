ae12d0aa5a12ed0bab0afe114eb2dcdf0d2d53ba
Q-engineering <info@qengineering.eu>
2022-06-10 09:52:39 +0200

    Update and rename OpenCV-4-5-6.sh to OpenCV-4-6-0.sh

--- OpenCV-4-6-0.sh
+++ OpenCV-4-6-0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-echo "Installing OpenCV 4.5.6 on your Raspberry Pi 64-bit OS"
+echo "Installing OpenCV 4.6.0 on your Raspberry Pi 64-bit OS"
 echo "It will take minimal 2.0 hour !"
 cd ~
 # install the dependencies
@@ -22,14 +22,14 @@ sudo apt-get install -y protobuf-compiler
 # download the latest version
 cd ~ 
 sudo rm -rf opencv*
-wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.6.zip 
-wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.6.zip 
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.6.0.zip 
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.6.0.zip 
 # unpack
 unzip opencv.zip 
 unzip opencv_contrib.zip 
 # some administration to make live easier later on
-mv opencv-4.5.6 opencv
-mv opencv_contrib-4.5.6 opencv_contrib
+mv opencv-4.6.0 opencv
+mv opencv_contrib-4.6.0 opencv_contrib
 # clean up the zip files
 rm opencv.zip
 rm opencv_contrib.zip
@@ -74,4 +74,4 @@ make clean
 sudo apt-get update
 
 echo "Congratulations!"
-echo "You've successfully installed OpenCV 4.5.6 on your Raspberry Pi 64-bit OS"
+echo "You've successfully installed OpenCV 4.6.0 on your Raspberry Pi 64-bit OS"
