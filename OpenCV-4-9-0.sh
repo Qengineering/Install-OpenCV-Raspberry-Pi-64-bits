9bc48b37c4ae676f6b5b890e3bf1d10637d532e0
Q-engineering <info@qengineering.eu>
2024-01-25 08:22:53 +0100

    Create OpenCV-4-9-0.sh

---
+++ OpenCV-4-9-0.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+set -e
+install_opencv () {
+echo ""
+
+case `cat /etc/debian_version` in
+10*) echo "Detecting Debian 10, Buster. "
+	;;
+11*) echo "Detecting Debian 11, Bullseye. "
+	;;
+12*) echo "Detecting Debian 12, Bookworm. "
+	;;
+esac
+
+echo ""
+echo "Installing OpenCV 4.8.0"
+echo "It will take minimal 2 hour !"
+cd ~
+# install the dependencies
+sudo apt-get install -y build-essential cmake git unzip pkg-config
+sudo apt-get install -y libjpeg-dev libtiff-dev libpng-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libgtk2.0-dev libcanberra-gtk* libgtk-3-dev
+sudo apt-get install -y libgstreamer1.0-dev gstreamer1.0-gtk3
+sudo apt-get install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-gl
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y python3-dev python3-numpy python3-pip
+sudo apt-get install -y libv4l-dev v4l-utils
+sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
+sudo apt-get install -y liblapack-dev gfortran libhdf5-dev
+sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
+sudo apt-get install -y protobuf-compiler
+
+#get TBB
+case `cat /etc/debian_version` in
+10*) sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev
+	;;
+11*) sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev
+	;;
+12*) sudo apt-get install -y libtbbmalloc2 libtbb-dev
+	;;
+esac
+
+# download the latest version
+cd ~ 
+sudo rm -rf opencv*
+git clone --depth=1 https://github.com/opencv/opencv.git
+git clone --depth=1 https://github.com/opencv/opencv_contrib.git
+
+# set install dir
+cd ~/opencv
+mkdir build
+cd build
+
+# run cmake
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+-D ENABLE_NEON=ON \
+-D WITH_OPENMP=ON \
+-D WITH_OPENCL=OFF \
+-D BUILD_TIFF=ON \
+-D WITH_FFMPEG=ON \
+-D WITH_TBB=ON \
+-D BUILD_TBB=ON \
+-D WITH_GSTREAMER=ON \
+-D BUILD_TESTS=OFF \
+-D WITH_EIGEN=OFF \
+-D WITH_V4L=ON \
+-D WITH_LIBV4L=ON \
+-D WITH_VTK=OFF \
+-D WITH_QT=OFF \
+-D WITH_PROTOBUF=ON \
+-D OPENCV_ENABLE_NONFREE=ON \
+-D INSTALL_C_EXAMPLES=OFF \
+-D INSTALL_PYTHON_EXAMPLES=OFF \
+-D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
+-D OPENCV_GENERATE_PKGCONFIG=ON \
+-D BUILD_EXAMPLES=OFF ..
+
+# run make
+make -j4
+sudo make install
+sudo ldconfig
+
+# cleaning (frees 320 MB)
+make clean
+sudo apt-get update
+
+echo "Congratulations!"
+echo "You've successfully installed OpenCV 4.8.0 on your Raspberry Pi 64-bit OS"
+}
+
+cd ~
+
+if [ -d ~/opencv/build ]; then
+  echo " "
+  echo "You have a directory ~/opencv/build on your disk."
+  echo "Continuing the installation will replace this folder."
+  echo " "
+  
+  printf "Do you wish to continue (Y/n)?"
+  read answer
+
+  if [ "$answer" != "${answer#[Nn]}" ] ;then 
+      echo "Leaving without installing OpenCV"
+  else
+      install_opencv
+  fi
+else
+    install_opencv
+fi
